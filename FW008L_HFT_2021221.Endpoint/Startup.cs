15ccff4324c1d65d15ff94d8d2775eb44697325b
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2021-11-20 19:36:17 +0100

    Setting up Startup.cs

--- FW008L_HFT_2021221.Endpoint/Startup.cs
+++ FW008L_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using FW008L_HFT_2021221.Data;
+using FW008L_HFT_2021221.Logic;
+using FW008L_HFT_2021221.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,13 +15,22 @@ namespace FW008L_HFT_2021221.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddTransient<IBookLogic, BookLogic>();
+            services.AddTransient<IPersonLogic, PersonLogic>();
+            services.AddTransient<IWriterLogic, WriterLogic>();
+
+            services.AddTransient<IBookRepository, BookRepository>();
+            services.AddTransient<IPersonRepository, PersonRepository>();
+            services.AddTransient<IWriterRepository, WriterRepository>();
+
+            services.AddTransient<LibraryDbContext, LibraryDbContext>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,11 +42,9 @@ namespace FW008L_HFT_2021221.Endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
+
     }
 }
