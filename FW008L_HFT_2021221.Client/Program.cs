26c5b58d5b2ef7f12d56efb8e9e5b2cc8e2eec41
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2021-11-22 13:52:05 +0100

    Formatting user interface.

--- FW008L_HFT_2021221.Client/Program.cs
+++ FW008L_HFT_2021221.Client/Program.cs
@@ -28,17 +28,8 @@ namespace FW008L_HFT_2021221.Client
 
             var  lastbook = rest.Get<KeyValuePair<string, int>>("stat/latestpublishedbooksbygeorges");
 
-            Console.WriteLine();
-            Console.WriteLine("TESZTELÉS");
-            foreach (KeyValuePair<string, int> kvp in lastbook)
-            {
-                Console.WriteLine("Author's name : {0,-20} || || The year it was published : {1,-20}",
-                    kvp.Key, kvp.Value);
-            }
 
-
-
-
+            
 
             //TESZT-----------------------------------------------------------------------
 
@@ -52,28 +43,124 @@ namespace FW008L_HFT_2021221.Client
             Console.WriteLine();
 
             Console.WriteLine("Please choose an option!");
-            Console.WriteLine("Type 1-10 a number.");
-            Console.WriteLine("[1]--Listing the books");
-            Console.WriteLine("[2]--Listing the people");
-            Console.WriteLine("[3]--Listing the writers");
+            Console.WriteLine("Type a number from 1 to 17!");
+
+            Console.WriteLine();
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+            
+
+            Console.WriteLine(string.Format("|| { 0,-20} ",numbers);
+
+
+
+            Console.WriteLine("[1]---Listing the books");
+            Console.WriteLine("[2]---Listing the people");
+            Console.WriteLine("[3]---Listing the writers");
+
+            Console.WriteLine("[4]---Adding a new book");
+            Console.WriteLine("[5]---Adding a new person");
+            Console.WriteLine("[6]---Adding a new writer");
+
+            Console.WriteLine("[7]---Updating an already existing book");
+            Console.WriteLine("[8]---Updating an already existing person");
+            Console.WriteLine("[9]---Updating an already existing writer");
+
+            Console.WriteLine("[10]--Deleting an already existing book");
+            Console.WriteLine("[11]--Deleting an already existing person"); //it is not what you might think it is
+            Console.WriteLine("[12]--Deleting an already existing writer"); //it is not what you might think it is
+
+            Console.WriteLine("[13]--Listing the hungarian readers");
+            Console.WriteLine("[14]--Listing how many books the people read under 18");
+            Console.WriteLine("[15]--Listing the latest books published by writers whose name is George");
+            Console.WriteLine("[16]--Listing those books which genre is Autobiography");
+            Console.WriteLine("[17]--Listing the 2 most productive writers");
+
 
             int answer = int.Parse(Console.ReadLine());
-            switch (answer)
+            if (answer == 1)
             {
-                case 1:
-                        foreach (var item in books)
-                        {
-                            Console.WriteLine(item);
-                        }
-                break;
-
-
-
-               
+                foreach (Person item in people)
+                {
+                    Console.WriteLine(people); // nicht gut
+                }
+            }
+            else if (answer == 2)
+            {
 
             }
-            Console.ReadLine();
+            else if (answer == 3)
+            {
 
+            }
+            else if (answer == 4)
+            {
+
+            }
+            else if (answer == 5)
+            {
+
+            }
+            else if (answer == 6)
+            {
+
+            }
+            else if (answer == 7)
+            {
+                
+            }
+            else if (answer == 8)
+            {
+
+            }
+            else if (answer == 9)
+            {
+
+            }
+            else if (answer == 10)
+            {
+
+            }
+            else if (answer == 11)
+            {
+
+            }
+            else if (answer == 12)
+            {
+
+            }
+            else if (answer == 13)
+            {
+
+            }
+            else if (answer == 14)
+            {
+
+            }
+            else if (answer == 15)
+            {
+                foreach (KeyValuePair<string, int> kvp in lastbook)
+                {
+                    Console.WriteLine("Author's name : {0,-20} || || The year it was published : {1,-20}",
+                        kvp.Key, kvp.Value);
+                }
+            }
+            else if (answer == 16)
+            {
+
+            }
+            else if (answer == 17)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("error, jo szam kell");
+            }
+
+
+
+            Console.ReadLine();
             #endregion userinterface
 
             ;
