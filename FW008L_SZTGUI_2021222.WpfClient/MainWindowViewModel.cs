30685a6b02c23b60be1ccdcbd1f65a50828e9e9c
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-09 23:55:20 +0200

    Setting up the MVVM.cs for basic testing.

---
+++ FW008L_SZTGUI_2021222.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FW008L_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FW008L_SZTGUI_2021222.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Book> Books { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Books = new RestCollection<Book>("http://localhost:48920/","book");
+        }
+
+    }
+}
