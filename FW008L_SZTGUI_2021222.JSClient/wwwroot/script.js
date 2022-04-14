471bf7adff99c516928a95c9e7bc5719f2c9b335
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-15 00:35:23 +0200

    Formatted a basic table output.

--- FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
+++ FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿let books = [];
+
+fetch('http://localhost:48920/book')
+    .then(x => x.json())
+    .then(y =>
+    {
+        books = y;
+        console.log(books);
+        display()
+    });
+
+
+function display()
+{
+    books.forEach(t =>
+    {
+        document.getElementById('results').innerHTML +=
+            "<tr><td>" + t.book_Id + "</td><td>" + t.title +
+            "</td><td>" + t.published + "</td><td>" + t.genre +
+            "</td><td>" + t.writer_Id + "</td><td>" + t.person_Id + "</td></tr>";
+        console.log(t.title);
+    });
+}
