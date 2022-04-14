f6aa28d9ccef9508fa04a660ab49989b721ee4d4
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-14 23:58:15 +0200

    Setting up the JS Client.

--- FW008L_SZTGUI_2021222.JSClient/Startup.cs
+++ FW008L_SZTGUI_2021222.JSClient/Startup.cs
@@ -28,6 +28,8 @@ namespace FW008L_SZTGUI_2021222.JSClient
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
