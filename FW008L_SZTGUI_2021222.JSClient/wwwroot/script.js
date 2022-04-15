4cf394af062d79dfb0db193b97992c9f9578281d
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-15 14:51:05 +0200

    Added the start function.

--- FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
+++ FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
@@ -22,6 +22,20 @@ function setupSignalR()
     start();
 }
 
+async function start()
+{
+    try
+    {
+        await connection.start();
+        console.log("SignalR Connected.");
+    }
+    catch (err)
+    {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
+
 
 async function getdata()
 {
