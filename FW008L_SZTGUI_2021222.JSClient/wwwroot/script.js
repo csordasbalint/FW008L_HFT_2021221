bf882c2191d5ae2a22789d1244fd136eab68833e
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-15 13:10:14 +0200

    Added the create function.

--- FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
+++ FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
@@ -21,3 +21,31 @@ function display()
         console.log(t.title);
     });
 }
+
+
+function create()
+{
+    let title = document.getElementById('title').value;
+    let published = document.getElementById('published').value;
+    let genre = document.getElementById('genre').value;
+    let writer_Id = document.getElementById('writer_Id').value;
+    let person_Id = document.getElementById('person_Id').value;
+
+    fetch('http://localhost:48920/book',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify(
+            {
+                title: title,
+                published: published,
+                genre: genre,
+                writer_Id: writer_Id,
+                person_Id: person_Id
+            }),
+        })
+        .then(response => response.json())
+        .then(data => { console.log('Success:', data); })
+        .catch((error) => {console.error('Error:', error); });
+
+}
