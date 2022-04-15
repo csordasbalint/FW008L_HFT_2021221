1dc18be5c57146b80cbf1e8bd413c25860e0c35b
balintcsordaas@stud.uni-obuda.hu <balintcsordaas@stud.uni-obuda.hu>
2022-04-15 14:14:20 +0200

    Fixing the create function.

--- FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
+++ FW008L_SZTGUI_2021222.JSClient/wwwroot/script.js
@@ -1,13 +1,17 @@
 ﻿let books = [];
+getdata();
 
-fetch('http://localhost:48920/book')
-    .then(x => x.json())
-    .then(y =>
-    {
-        books = y;
-        console.log(books);
-        display()
-    });
+async function getdata()
+{
+    await fetch('http://localhost:48920/book')
+        .then(x => x.json())
+        .then(y =>
+        {
+            books = y;
+            console.log(books);
+            display()
+        });
+}
 
 
 function display()
@@ -18,34 +22,66 @@ function display()
             "<tr><td>" + t.book_Id + "</td><td>" + t.title +
             "</td><td>" + t.published + "</td><td>" + t.genre +
             "</td><td>" + t.writer_Id + "</td><td>" + t.person_Id + "</td></tr>";
-        console.log(t.title);
     });
 }
 
 
 function create()
 {
-    let title = document.getElementById('title').value;
-    let published = document.getElementById('published').value;
-    let genre = document.getElementById('genre').value;
-    let writer_Id = document.getElementById('writer_Id').value;
-    let person_Id = document.getElementById('person_Id').value;
+    let book_title = document.getElementById('title').value;
+    let book_published = document.getElementById('published').value;
+    let book_genre = document.getElementById('genre').value;
+    let book_writer_Id = document.getElementById('writer_Id').value;
+    let book_person_Id = document.getElementById('person_Id').value;
 
     fetch('http://localhost:48920/book',
         {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(
-            {
-                title: title,
-                published: published,
-                genre: genre,
-                writer_Id: writer_Id,
-                person_Id: person_Id
-            }),
+                {
+                    title: book_title,
+                    published: book_published,
+                    genre: book_genre,
+                    writer_Id: book_writer_Id,
+                    person_Id: book_person_Id
+                })
         })
-        .then(response => response.json())
-        .then(data => { console.log('Success:', data); })
-        .catch((error) => {console.error('Error:', error); });
-
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
+
+
+
+
+//function create2() {
+//    let title = document.getElementById('title').value;
+//    let published = document.getElementById('published').value;
+//    let genre = document.getElementById('genre').value;
+//    let writer_Id = document.getElementById('writer_Id').value;
+//    let person_Id = document.getElementById('person_Id').value;
+
+//    fetch('http://localhost:48920/book',
+//        {
+//            method: 'POST',
+//            headers: { 'Content-Type': 'application/json', },
+//            body: JSON.stringify(
+//                {
+//                    title: title,
+//                    published: published,
+//                    genre: genre,
+//                    writer_Id: writer_Id,
+//                    person_Id: person_Id
+//                }),
+//        })
+//        .then(response => response)
+//        .then(data => {
+//            console.log('Success:', data);
+//            getData();
+//        })
+//        .catch((error) => { console.error('Error:', error); });
+//}
